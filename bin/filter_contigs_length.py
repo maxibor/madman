d7ba89d54f3acb2aa49e1a835f9ca0d87199fc02
maxibor <maxime.borry@gmail.com>
2020-03-12 16:46:31 +0100

    functionalize and rename

--- bin/filter_contigs_length.py
+++ bin/filter_contigs_length.py
@@ -9,9 +9,9 @@ from textwrap import wrap
 def _get_args():
     '''This function parses and return arguments passed in'''
     parser = argparse.ArgumentParser(
-        prog='fasta_filter_length',
-        description='filterFastaByLength')
-    parser.add_argument('fasta', help="path to input fasta file")
+        prog='fasta_cotnigs_length',
+        description='filterContigsByLength')
+    parser.add_argument('fasta', help="path to input fasta contig file")
     parser.add_argument(
         '-min',
         default=1,
@@ -49,6 +49,39 @@ def get_basename(file_name):
         basename = file_name.split(".")[0]
     return(basename)
 
+def parse_fasta(fasta_file):
+        """Parse a fasta file
+
+        Args:
+            fasta_file (str): Path to fasta file
+        Returns:
+            (dict): Seqname as key, sequence as value
+        """   
+
+        fastadict = {}
+        with open(fasta_file, "r") as f:
+            for line in f:
+                line = line.rstrip()
+                if line.startswith(">"):
+                    seqname = line
+                    fastadict[seqname] = []
+                else:
+                    fastadict[seqname].append(line)
+        return(fastadict)
+
+def write_fasta(fasta_dict, outfile):
+    """Write fasta to file
+
+    Args:
+        fasta_dict (dict): Seqname as key, sequence as value
+        outfile (str): Path to output file
+    """    
+    with open(outfile, "w") as fw:
+        for seq in list(fasta_dict.keys()):
+            fw.write(seq + "\n")
+            fw.write("\n".join(wrap(("".join(fasta_filtered_dict[seq])), width = 80)))
+            fw.write("\n")
+
 def process_sequence(thekey, thedict, seqmin, seqmax):
     resdict = {}
     if len("".join(thedict[thekey])) >= seqmin and len("".join(thedict[thekey])) <= seqmax:
@@ -65,16 +98,7 @@ if __name__ == "__main__":
         OUTFILE = basename + ".filtered.fa"
 
     
-    fastadict = {}
-    fasta_filtered_dict = {}
-    with open(INFILE, "r") as f:
-        for line in f:
-            line = line.rstrip()
-            if line.startswith(">"):
-                seqname = line
-                fastadict[seqname] = []
-            else:
-                fastadict[seqname].append(line)
+    fastadict = parse_fasta(INFILE)
 
     process_partial = partial(process_sequence, 
                               thedict = fastadict, 
@@ -89,13 +113,11 @@ if __name__ == "__main__":
         for seq in list(fastadict.keys()):
             res.append(process_sequence(thekey = seq, thedict=fastadict, seqmin=SEQMIN, seqmax=SEQMAX))
 
+    fasta_filtered_dict = {}
     for d in res:
         if d:
             fasta_filtered_dict.update(d)
 
     # print(fasta_filtered_dict)
-    with open(OUTFILE, "w") as fw:
-        for seq in list(fasta_filtered_dict.keys()):
-            fw.write(seq + "\n")
-            fw.write("\n".join(wrap(("".join(fasta_filtered_dict[seq])), width = 80)))
-            fw.write("\n")
+
+    write_fasta(fasta_filtered_dict, OUTFILE)
